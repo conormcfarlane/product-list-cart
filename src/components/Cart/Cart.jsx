52cc1eb34e5aed146aee83e77a9c660a12114d4a
conormcfarlane <conormcf98@gmail.com>
2025-04-27 10:16:24 +1000

    Finished Cart functionality and styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Cart.css'
 import emptyCartImg from '/images/illustration-empty-cart.svg'
+import removeButton from '/images/icon-remove-item.svg'
 
 export default function Cart({cart, addToCart, removeFromCart}) {
   const total = cart.reduce((sum, item) => sum + item.price * item.quantity,0)
+  
   
   return (
     <>
@@ -22,21 +24,24 @@ export default function Cart({cart, addToCart, removeFromCart}) {
                   <div className="cart-content-left">
                 <div className="name-container">
                  <p>{dessert.name}</p> 
-                 <p>{dessert.id}</p>
                 </div>
                 <div className="price-container">
-                  <p>{dessert.quantity}x</p>
-                  <p>@ ${dessert.price}</p>
+                  <p className='text-preset-4-bold'>{dessert.quantity}x</p>
+                  <p className='text-preset-4'>@ ${dessert.price.toFixed(2)}</p>
+                  <p className='text-preset-4-bold'>${dessert.price * dessert.quantity}</p>
                 </div>
               </div>
               <div className="cart-content-right">
-                <button onClick={() => removeFromCart(dessert.id)}>REMOVE</button>
+                <button onClick={() => removeFromCart(dessert.id)}>
+                  <img src={removeButton} alt="" />
+                </button>
               </div>
             </div>
               ))}
               
-              <div className='cart-Total'>
-                <p>Total: ${total.toFixed(2)}</p>
+              <div className='cart-total'>
+                <p className='text-preset-4'>Order Total: </p>
+                <p className='text-preset-2'>${total.toFixed(2)}</p>
               </div>
             </div>
             
